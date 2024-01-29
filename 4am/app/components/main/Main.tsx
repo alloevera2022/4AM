23616a948cbe5fd1f15e7138872d49e4da3a8ea0
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-01-29 10:49:14 +0000

    create mainblock

---
+++ 4am/app/components/main/Main.tsx
@@ -0,0 +1,30 @@
+import "./main.css";
+
+export default function Main() {
+    return (
+      <section className="Main">
+    <div className="main__content">
+        <div className="main__left-block">
+            <div className="main__left-block-content">  <img className="logo" src="#!" alt="Logo"/>
+            <h1>Keep your favourite items now digitally</h1>
+            <h2>AXXIS transforms iconic clothing and footwear into unique NFTs. Own a piece of fashion, trade with ease, and step into a world where every item tells a story.</h2>
+            <button className="button">Get yours</button>
+
+            <div className="main__left-block-content-links">
+                <ul>
+                    <li><a href="#">info@AXXISproject.io</a></li>
+                    <li><a href="#">@AXXIS</a></li>
+                    <li><a href="#">@AXXIS_project</a></li>
+                </ul>
+            </div></div>
+         
+        </div>
+        <div className="main__right-block">
+            <img src="" alt="Image"/>
+        </div>
+    </div>
+
+    </section >
+    );
+  }
+  
--- 4am/app/page.tsx
+++ 4am/app/page.tsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Main />
 
     </main>
   );
