6a2beff62d4d00dcf80a8a1ad941859c02acbaac
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-01-29 10:27:51 +0000

    create nextjs + add header

---
+++ 4am/app/components/Header.tsx
@@ -0,0 +1,16 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+      <header className="main-header">
+      <nav className="navigation-links">
+        <a href="#!">About</a>
+        <a href="#!">Presale</a>
+        <a href="#!">Whitepaper</a>
+        <a href="#!">F.A.Q.</a>
+      </nav>
+      <button className="action-button">Coming soon</button>
+    </header>
+    );
+  }
+  
