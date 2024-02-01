503116d8d9345a318d0f5a5a6b9a52705fdd60d8
Alex N <leonelleo@yandex.ru>
2024-02-01 18:36:19 +0300

    fix connectwallet on mobile screen

--- 4am/app/components/main/Main.tsx
+++ 4am/app/components/main/Main.tsx
@@ -1,35 +1,70 @@
 import "./main.css";
-import mainLogo from '../public/img/logo_main.webp';
-import emailLogo from '../public/img/social_logo_svg/email_logo.svg';
-import telegramLogo from '../public/img/social_logo_svg/telegram_logo.svg';
-import xLogo from '../public/img/social_logo_svg/x_logo.svg';
+import mainLogo from "../public/img/logo_main.webp";
+import emailLogo from "../public/img/social_logo_svg/email_logo.svg";
+import telegramLogo from "../public/img/social_logo_svg/telegram_logo.svg";
+import xLogo from "../public/img/social_logo_svg/x_logo.svg";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-
-
-
+import { WalletButton } from "@rainbow-me/rainbowkit";
 
 export default function Main() {
-    return (
-      <section className="Main">
-    <div className="main__content">
+  return (
+    <section className="Main">
+      <div className="main__content">
         <div className="main__left-block">
-            <div className="main__left-block-content">  <img src={mainLogo.src} className='main_logo' alt="Logo"/>
+          <div className="main__left-block-content">
+            {" "}
+            <img src={mainLogo.src} className="main_logo" alt="Logo" />
             <h1>Keep your favourite items now digitally</h1>
-            <h2>AXXIS transforms iconic clothing and footwear into unique NFTs. Own a piece of fashion, trade with ease, and step into a world where every item tells a story.</h2>
-            <div className="main_button"><ConnectButton label="Get yours"/></div>
+            <h2>
+              AXXIS transforms iconic clothing and footwear into unique NFTs.
+              Own a piece of fashion, trade with ease, and step into a world
+              where every item tells a story.
+            </h2>
+            <div className="main_button">
+              <ConnectButton
+                label="Get yours"
+                chainStatus="icon" 
+                accountStatus={{
+                  smallScreen: "avatar",
+                  largeScreen: "full",
+                }}
+                showBalance={{
+                  smallScreen: false,
+                  largeScreen: true,
+                }}
+              />
+            </div>
             {/* <div className="main_button"><button><a href="https://twitter.com/XXS_crypto">Get yours</a></button></div> */}
             <div className="main__left-block-content-links">
-                    <div className="main-link"><a href="mailto:info@AXXISproject.io"><img src={emailLogo.src} className="sl_logos"/></a><a href="mailto:info@AXXISproject.io"><p className="sl_text">info@AXXISproject.io</p></a></div>
-                    <div className="main-link"><a href="https://t.me/AXXISinfo"><img src={telegramLogo.src} className="sl_logos"/></a><a href="https://t.me/AXXISinfo"><p className="sl_text">@AXXIS</p></a></div>
-                    <div className="main-link"><a href="https://twitter.com/XXS_crypto"><img src={xLogo.src} className="sl_logos"/></a><a href="https://twitter.com/XXS_crypto"><p className="sl_text">@AXXIS_project</p></a></div>                 
+              <div className="main-link">
+                <a href="mailto:info@AXXISproject.io">
+                  <img src={emailLogo.src} className="sl_logos" />
+                </a>
+                <a href="mailto:info@AXXISproject.io">
+                  <p className="sl_text">info@AXXISproject.io</p>
+                </a>
+              </div>
+              <div className="main-link">
+                <a href="https://t.me/AXXISinfo">
+                  <img src={telegramLogo.src} className="sl_logos" />
+                </a>
+                <a href="https://t.me/AXXISinfo">
+                  <p className="sl_text">@AXXIS</p>
+                </a>
+              </div>
+              <div className="main-link">
+                <a href="https://twitter.com/XXS_crypto">
+                  <img src={xLogo.src} className="sl_logos" />
+                </a>
+                <a href="https://twitter.com/XXS_crypto">
+                  <p className="sl_text">@AXXIS_project</p>
+                </a>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        <div className="main__right-block">
-        </div>
-    </div>
-
-    </section >
-    );
-  }
-  
+        <div className="main__right-block"></div>
+      </div>
+    </section>
+  );
+}
--- 4am/app/providers.tsx
+++ 4am/app/providers.tsx
@@ -14,13 +14,13 @@ import {
   ledgerWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { polygonMumbai, sepolia, lineaTestnet } from "wagmi/chains";
+import { mainnet, polygonMumbai, sepolia, lineaTestnet, optimism} from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 const projectId = "effed73ceee99d947720919370f0f035";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [polygonMumbai, sepolia, lineaTestnet],
+  [mainnet, polygonMumbai, sepolia, lineaTestnet, optimism],
   [publicProvider()]
 );
 
@@ -55,7 +55,7 @@ const wagmiConfig = createConfig({
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <WagmiConfig config={wagmiConfig}>
+    <WagmiConfig config={wagmiConfig} >
       <RainbowKitProvider 
          theme={darkTheme({
           accentColor: '#dbdbdb',
@@ -66,6 +66,7 @@ const Providers = ({ children }: { children: ReactNode }) => {
         })}
         chains={chains}
         appInfo={demoAppInfo}
+        coolMode
       >
         {children}
       </RainbowKitProvider>
